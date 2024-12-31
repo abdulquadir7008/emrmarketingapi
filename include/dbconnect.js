09f0aacb2d7610ed746f840a438c1d4e2edc73e6
quadir <quadir@quadirs-MacBook-Air.local>
2024-12-31 12:43:12 +0530

    myql remote connect

--- include/dbconnect.js
+++ include/dbconnect.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const dbHOST = process.env.DBHOST;
-const sequelize = new Sequelize('emr_marketing', 'root', '', {
-    host: dbHOST,
-    dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+    `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
+    {
+      dialect: 'mysql',
+      dialectModule: require('mysql2'), 
+      logging: false,
+    }
+  );
+  sequelize.authenticate()
+  .then(() => {
+    console.log('Connection established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
